65cb8dc6088e6a234b9a2839dd465821bd05b1ce
morningstar004 <pranjalkumar000007@gmail.com>
2025-10-12 13:55:01 +0530

    initial commit ,project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+// import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      {/* <Navbar/> */}
+      <div className=" rounded-full bg-blue-400 text-white p-6 m-4 text-center">
+      🚀 Tailwind + React = Love 💙
+      </div>
+    </>
+  )
+}
+
+export default App
